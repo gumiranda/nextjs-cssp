126386afe6b7aa4cb34580cb1b35008bf54fd2f3
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-07-30 08:13:25 -0300

    refactor: remove unused imports in HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
-
- 
+  
 export default async function HomePage() {
   // ✅ Pode fazer fetch diretamente
   const post = await fetch('https://api.adviceslip.com/advice')
